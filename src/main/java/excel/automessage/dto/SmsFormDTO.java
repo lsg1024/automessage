4ced648c0ef8302243bfb430955ae855b9b11098
JS <zks145@naver.com>
2024-05-14 18:53:48 +0900

    feat : 단일 가게 html 및 css 추가, 수정

--- src/main/java/excel/automessage/dto/SmsFormDTO.java
+++ src/main/java/excel/automessage/dto/SmsFormDTO.java
@@ -3,13 +3,20 @@ package excel.automessage.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter @Setter
 public class SmsFormDTO {
+    public Map<String, List<String>> smsForm;
+    public Map<String, String> smsPhone;
+    public List<String> missingStores;
 
-    HashMap<String, List<String>> smsForm = new HashMap<>();
-    HashMap<String, String> smsPhone = new HashMap<>();
-
+    public SmsFormDTO() {
+        this.smsForm = new HashMap<>();
+        this.smsPhone = new HashMap<>();
+        this.missingStores = new ArrayList<>();
+    }
 }
