6b086f3c58df2beb043fdf6cd905b14320cc0cf5
JS <zks145@naver.com>
2024-06-20 23:31:13 +0900

    refactor: 컨트롤러 위치 변경

--- src/main/java/excel/automessage/controller/HomeController.java
+++ src/main/java/excel/automessage/controller/HomeController.java
@@ -22,39 +22,12 @@ public class HomeController {
         return "storeForm/storeSelect";
     }
 
-    // 새로운 상점 (엑셀 입력)
-    @RequestMapping("/new/stores")
-    public String excelStore() {
-        log.info("excelStore controller");
-        return "storeForm/storeUpload";
+    @RequestMapping("/sms")
+    public String sms() {
+        log.info("sms controller");
+        return "smsForm/smsUpload";
     }
 
-    // 새로운 상점 (직접 입력)
-    @RequestMapping("/new/store")
-    public String newStore() {
-        log.info("newStore controller");
-        return "storeForm/storeInput";
-    }
-//
-//    // 추가 상점 (미등록 상점)
-//    @RequestMapping("/store/add")
-//    public String addStore() {
-//        log.info("addStore Controller");
-//        return "storeForm/missingStore";
-//    }
-//
-//    @RequestMapping("/stores")
-//    public String storeList() {
-//        log.info("stores controller");
-//        return "storeForm/storeList";
-//    }
-//
-//    @RequestMapping("/sms")
-//    public String sms() {
-//        log.info("sms controller");
-//        return "smsForm/smsUpload";
-//    }
-//
     @RequestMapping("/sms/content")
     public String smsContent() {
         log.info("smsContent Controller");
