57800a6cac13f5757cc5df47ab8d616d8a639a95
lsg1024 <zks145@naver.com>
2024-06-28 16:56:55 +0900

    feat: 저장 시간 클래스 추가

--- src/main/java/excel/automessage/AutomessageApplication.java
+++ src/main/java/excel/automessage/AutomessageApplication.java
@@ -2,12 +2,22 @@ package excel.automessage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 public class AutomessageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutomessageApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
