d7316d348f8642a5445aefdda204d5136fcfcc85
lsg1024 <zks145@naver.com>
2024-06-28 16:56:55 +0900

    feat: 저장 시간 클래스 추가

---
+++ src/main/java/excel/automessage/domain/auditing/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package excel.automessage.domain.auditing;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private String createDate;
+
+    @LastModifiedDate
+    private String lastModifiedDate;
+}
