e8770559c7ccb0d0de522039963dca88887554f0
lsg1024 <zks145@naver.com>
2024-07-01 00:29:30 +0900

    feat: 가게 생성 성공, 실패 코드 작성

--- src/test/java/excel/automessage/service/StoreServiceTest.java
+++ src/test/java/excel/automessage/service/StoreServiceTest.java
@@ -1,25 +1,20 @@
 package excel.automessage.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import excel.automessage.domain.Store;
 import excel.automessage.dto.store.StoreDTO;
 import excel.automessage.dto.store.StoreListDTO;
+import excel.automessage.repository.StoreRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Slf4j
@@ -29,84 +24,75 @@ public class StoreServiceTest {
     private StoreService storeService;
 
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    private StoreRepository storeRepository;
 
-    private StoreListDTO storeListDTO;
-
-    @BeforeEach
-    public void setup() {
-        storeListDTO = new StoreListDTO();
-        List<StoreDTO.Save> storeDTOList = new ArrayList<>();
-
-        for (int i = 0; i < 900; i++) {  // 테스트 데이터 생성
-            StoreDTO.Save storeDTO = new StoreDTO.Save();
-            storeDTO.setName("Store" + i);
-            storeDTO.setPhone("010-1234-" + String.format("%04d", i));
-            storeDTOList.add(storeDTO);
-        }
-
-        storeListDTO.setStores(storeDTOList);
-    }
-
-    @Test
+    // 가게 등록 (직접 등록) 성공, 실패
     @Transactional
-    @DisplayName("DB 저장")
-    public void testSaveAllToDatabase() {
-        long startTime = System.currentTimeMillis();
-
-        storeService.saveAll(storeListDTO);
-
-        long endTime = System.currentTimeMillis();
-        long duration = endTime - startTime;
-
-        log.info("DB 저장 시간: {}ms", duration);
-    }
-
+    @DisplayName("가게 등록 성공(직접등록)")
     @Test
-    @Transactional
-    @DisplayName("Redis 저장")
-    public void testSaveAllToRedisAndDatabaseAsync() {
-        long redisStartTime = System.currentTimeMillis();
+    void createStore() {
+        //given
+        StoreListDTO storeListDTO = new StoreListDTO();
 
-        String key = "testStoresKey";
+        StoreDTO store_1 = new StoreDTO();
+        store_1.setName("홍길동");
+        store_1.setPhone("010-1234-5678");
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            String serializedData = objectMapper.writeValueAsString(storeListDTO);
-            redisTemplate.opsForValue().set(key, serializedData);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to serialize StoreListDTO to JSON", e);
-        }
+        StoreDTO store_2 = new StoreDTO();
+        store_2.setName("김모이");
+        store_2.setPhone("01098765432");
 
-        long redisEndTime = System.currentTimeMillis();
-        long redisDuration = redisEndTime - redisStartTime;
-        log.info("Redis 저장 시간: {}ms", redisDuration);
+        storeListDTO.getStores().add(store_1);
+        storeListDTO.getStores().add(store_2);
 
-        // Redis에 저장된 데이터를 DB에 저장하는 비동기 처리 실행
-        long dbStartTime = System.currentTimeMillis();
-        CompletableFuture<Void> future = storeService.saveAllToDBAsync(key);
+        //when
+        StoreListDTO saveResult = storeService.saveAll(storeListDTO);
 
-        // 비동기 작업 중 Redis 데이터에 접근
-        String redisDataDuringDBSave = (String) redisTemplate.opsForValue().get(key);
-        assertNotNull(redisDataDuringDBSave, "Redis 데이터에 접근할 수 있어야 합니다.");
+        //then
+        Optional<Store> saveStore_1 = storeRepository.findById(saveResult.getStores().get(0).getId());
+        assertTrue(saveStore_1.isPresent());
+        assertEquals(store_1.getName(), saveStore_1.get().getStoreName());
+        assertEquals("01012345678", saveStore_1.get().getStorePhoneNumber());
 
-        try {
-            // DB 저장이 완료될 때까지 대기 5분
-            future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException("Failed to save data to DB", e);
-        }
+        Optional<Store> saveStore_2 = storeRepository.findById(saveResult.getStores().get(1).getId());
+        assertTrue(saveStore_2.isPresent());
+        assertEquals(store_2.getName(), saveStore_2.get().getStoreName());
+        assertEquals("01098765432", saveStore_2.get().getStorePhoneNumber());
 
-        long dbEndTime = System.currentTimeMillis();
-        long dbDuration = dbEndTime - dbStartTime;
-        log.info("DB 저장 시간: {}ms", dbDuration);
-
-        // 전체 실행 시간을 계산하여 로그에 출력
-        long totalTime = redisDuration + dbDuration;
-        log.info("전체 실행 시간: {}ms", totalTime);
-
-        // Redis 데이터가 삭제되었는지 확인
-        String redisDataAfterDBSave = (String) redisTemplate.opsForValue().get(key);
-        assertNull(redisDataAfterDBSave, "DB 저장 후 Redis 데이터는 삭제되어야 합니다.");
     }
+
+    @Transactional
+    @DisplayName(("가게 등록 실패(직접등록)"))
+    @Test
+    void createFailStore() {
+        //given
+        StoreListDTO storeListDTO = new StoreListDTO();
+
+        StoreDTO store_1 = new StoreDTO();
+        store_1.setName("홍길동");
+        store_1.setPhone("010/1234/5667");
+
+        StoreDTO store_2 = new StoreDTO();
+        store_2.setName("김모이");
+        store_2.setPhone("010 9876 5432");
+
+        storeListDTO.getStores().add(store_1);
+        storeListDTO.getStores().add(store_2);
+
+        //when
+        //then
+        assertThrows(IllegalStateException.class, () -> {
+            storeService.saveAll(storeListDTO);
+        }, "옳바른 번호를 입력해주세요.");
+
+    }
+
+    // 가게 등록 (엑셀 등록) 성공, 실패
+
+    // 가게 수정 성공, 실패
+
+    // 가게 삭제 성공, 실패
+
+    // 가게 검색 성공, 실패
+
 }
