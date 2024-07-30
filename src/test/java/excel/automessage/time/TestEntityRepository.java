2aab3681668d88a4c73a1ce0774113958e049c33
JS <zks145@naver.com>
2024-07-30 22:34:47 +0900

    test: 시간 및 가게 테스트 코드

---
+++ src/test/java/excel/automessage/time/TestEntityRepository.java
@@ -0,0 +1,6 @@
+package excel.automessage.time;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
+}
