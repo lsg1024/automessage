d46df2a85fa745019930a0680fb8c95e20700e5e
JS <zks145@naver.com>
2024-07-30 22:46:26 +0900

    chore: 파일 경로 변경으로 인한 수정

--- src/main/java/excel/automessage/dto/store/StoreDTO.java
+++ src/main/java/excel/automessage/dto/store/StoreDTO.java
@@ -1,44 +1,22 @@
 package excel.automessage.dto.store;
 
-import excel.automessage.entity.Store;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
 
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class StoreDTO {
 
     Long id;
+
+    @NotBlank(message = "사용자 이름을 입력해주세요.")
     String name;
+
+    @Pattern(regexp = "^01(?:0|1|[0-1])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
     String phone;
 
-    public Store toEntity() {
-        return Store.builder()
-                .storeName(name)
-                .storePhoneNumber(phone)
-                .build();
-    }
 }
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.Pattern;
-//import lombok.*;
-//
-//@Getter @Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class StoreDTO {
-//
-//    Long id;
-//
-//    @NotBlank(message = "사용자 이름을 입력해주세요.")
-//    String name;
-//
-//    @Pattern(regexp = "^01(?:0|1|[0-1])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
-//    String phone;
-//
-//}
 
