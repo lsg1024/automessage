c2bf3d1d0517a17092afd0c1a064549c03e39d43
JS <zks145@naver.com>
2024-07-30 22:45:45 +0900

    chore: 파일 경로 변경으로 인한 수정

--- src/main/java/excel/automessage/dto/store/StoreDTO.java
+++ src/main/java/excel/automessage/dto/store/StoreDTO.java
@@ -1,6 +1,6 @@
 package excel.automessage.dto.store;
 
-import excel.automessage.domain.Store;
+import excel.automessage.entity.Store;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,5 +21,24 @@ public class StoreDTO {
                 .storePhoneNumber(phone)
                 .build();
     }
-
 }
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.Pattern;
+//import lombok.*;
+//
+//@Getter @Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class StoreDTO {
+//
+//    Long id;
+//
+//    @NotBlank(message = "사용자 이름을 입력해주세요.")
+//    String name;
+//
+//    @Pattern(regexp = "^01(?:0|1|[0-1])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
+//    String phone;
+//
+//}
+
