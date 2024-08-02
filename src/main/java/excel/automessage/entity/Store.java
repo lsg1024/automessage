df887434812b264e6ea94496556131534da4736e
JS <zks145@naver.com>
2024-08-02 15:12:35 +0900

    refactor: soft delete 추가

--- src/main/java/excel/automessage/entity/Store.java
+++ src/main/java/excel/automessage/entity/Store.java
@@ -5,18 +5,23 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE Store SET deleted = true where store_id = ?")
+@SQLRestriction("deleted = false")
 public class Store {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "storeId")
-    Long storeId;
+    private Long storeId;
     @Column(name = "storeName")
-    String storeName;
-    String storePhoneNumber;
+    private String storeName;
+    private String storePhoneNumber;
+    private boolean deleted = Boolean.FALSE;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "historyId")
