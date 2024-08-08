84d5695894f203a80843a59897cf0f232e39adf4
JS <zks145@naver.com>
2024-08-08 18:52:12 +0900

    feat: spring security

---
+++ src/main/java/excel/automessage/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package excel.automessage.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login", "/register", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/automessage/**").authenticated()
+                        .anyRequest().authenticated()
+                );
+
+        http.exceptionHandling((except) -> except
+                .accessDeniedPage("/login"));
+
+        http.formLogin((auth) -> auth.loginPage("/login")
+                .defaultSuccessUrl("/automessage", true)
+//                .loginProcessingUrl("/loginProc")
+                .permitAll());
+
+        http.rememberMe((remember) -> remember
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(84000 * 2)
+                );
+
+
+        return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
