c6b051b1a086c44ffd6de1521da6920f4a10634f
lsg1024 <zks145@naver.com>
2024-08-09 22:41:44 +0900

    feat: Redis 이용한 로그인 유지 (미완)

--- src/main/java/excel/automessage/config/SecurityConfig.java
+++ src/main/java/excel/automessage/config/SecurityConfig.java
@@ -1,47 +1,73 @@
 package excel.automessage.config;
 
-
+import excel.automessage.config.handler.CustomLoginSuccessHandler;
+import excel.automessage.config.handler.CustomLogoutSuccessHandler;
+import excel.automessage.service.RedisTokenService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final CustomLoginSuccessHandler customLoginSuccessHandler;
+    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(AbstractHttpConfigurer::disable);
-
         http.authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login", "/signup", "/register", "/css/**", "/js/**").permitAll()
-                        .requestMatchers("/automessage/**").authenticated()
+                        .requestMatchers("/", "/login", "/signup", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/automessage/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated());
 
         http.exceptionHandling((except) -> except
                 .accessDeniedPage("/login"));
 
-        http.formLogin((auth) -> auth.loginPage("/login")
-                .defaultSuccessUrl("/automessage", true)
-//                .loginProcessingUrl("/loginProc")
+        http.formLogin((auth) -> auth
+                .loginPage("/login")
+                .usernameParameter("memberId")
+                .passwordParameter("memberPassword")
+                .loginProcessingUrl("/login")
+                .successHandler(customLoginSuccessHandler)
+                .permitAll());
+
+        http.logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(customLogoutSuccessHandler)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
                 .permitAll());
 
         http.rememberMe((remember) -> remember
                 .rememberMeParameter("remember")
-                .tokenValiditySeconds(84000 * 2));
+                .tokenValiditySeconds(3 * 24 * 60 * 60) // 3일 동안 유효한 쿠키
+                .tokenRepository(persistentTokenRepository()));
 
         return http.build();
+    }
+
+
+    // Redis
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        return new RedisTokenService(redisTemplate);
     }
 
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
