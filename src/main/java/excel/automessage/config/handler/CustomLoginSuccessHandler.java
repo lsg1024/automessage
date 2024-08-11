3d5cc1b0b2003522b3267ac667205dbfc00596c9
lsg1024 <zks145@naver.com>
2024-08-11 14:19:18 +0900

    feat: 미인증 사용자 로그인 거부

--- src/main/java/excel/automessage/config/handler/CustomLoginSuccessHandler.java
+++ src/main/java/excel/automessage/config/handler/CustomLoginSuccessHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,12 +18,9 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
-    private final RedisTokenService redisTokenService;
-    private RememberMeServices rememberMeServices;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -45,4 +43,6 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
         response.sendRedirect("/automessage");
     }
+
+
 }
--- src/main/java/excel/automessage/service/CustomMemberDetailService.java
+++ src/main/java/excel/automessage/service/CustomMemberDetailService.java
@@ -4,6 +4,7 @@ import excel.automessage.dto.members.CustomMemberDetails;
 import excel.automessage.entity.Members;
 import excel.automessage.repository.MembersRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +14,7 @@ import javax.management.relation.RoleNotFoundException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomMemberDetailService implements UserDetailsService {
 
     private final MembersRepository membersRepository;
@@ -21,12 +23,18 @@ public class CustomMemberDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
         Members members = membersRepository.findByMemberId(memberId);
 
+
+
         if (members == null) {
+            log.info("사용자를 찾을 수 없습니다.");
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다:" + memberId);
         }
 
-        if (String.valueOf(members.getRole()).equals("ROLE_WAIT")) {
-            return (UserDetails) new RoleNotFoundException("미승인 사용자 입니다:" + memberId);
+        log.info("members.getRole() {}", members.getRole());
+
+        if (String.valueOf(members.getRole()).equals("WAIT")) {
+            log.info("미승인 사용자 입니다. ROLE_USER {}", memberId);
+            throw  new UsernameNotFoundException("미승인 사용자 입니다:" + memberId);
         }
 
         return new CustomMemberDetails(members); //JSESSIONID 쿠키 저장
