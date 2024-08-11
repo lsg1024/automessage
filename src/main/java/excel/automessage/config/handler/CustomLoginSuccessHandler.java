f7bf47cecc57ec47a56eb2ce7af616daa3519edc
JS <zks145@naver.com>
2024-08-11 12:51:56 +0900

    feat: redis 로그인 정보 저장 (역직렬화 커스텀 클래스 생성)

--- src/main/java/excel/automessage/config/handler/CustomLoginSuccessHandler.java
+++ src/main/java/excel/automessage/config/handler/CustomLoginSuccessHandler.java
@@ -1,20 +1,48 @@
 package excel.automessage.config.handler;
 
-import jakarta.servlet.FilterChain;
+import excel.automessage.service.RedisTokenService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final RedisTokenService redisTokenService;
+    private RememberMeServices rememberMeServices;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String role = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElse("");
+
+        log.info("user role = {}", role);
+
+        if (role.equals("ROLE_WAIT")) {
+            HttpSession session = request.getSession(false);
+            if (session != null) {
+                session.invalidate();
+            }
+        }
+
         response.sendRedirect("/automessage");
     }
 }
--- src/main/java/excel/automessage/service/CustomMemberDetailService.java
+++ src/main/java/excel/automessage/service/CustomMemberDetailService.java
@@ -3,13 +3,13 @@ package excel.automessage.service;
 import excel.automessage.dto.members.CustomMemberDetails;
 import excel.automessage.entity.Members;
 import excel.automessage.repository.MembersRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import javax.management.relation.RoleNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +22,11 @@ public class CustomMemberDetailService implements UserDetailsService {
         Members members = membersRepository.findByMemberId(memberId);
 
         if (members == null) {
-            throw  new UsernameNotFoundException("사용자를 찾을 수 없습니다:" + memberId);
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다:" + memberId);
+        }
+
+        if (String.valueOf(members.getRole()).equals("ROLE_WAIT")) {
+            return (UserDetails) new RoleNotFoundException("미승인 사용자 입니다:" + memberId);
         }
 
         return new CustomMemberDetails(members); //JSESSIONID 쿠키 저장
