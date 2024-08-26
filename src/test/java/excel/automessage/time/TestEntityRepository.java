208197db2f74949addf0924914a0fffb9e52d66d
JS <zks145@naver.com>
2024-08-26 10:30:38 +0900

    delete: 임시 파일 제거

--- src/test/java/excel/automessage/time/TestEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package excel.automessage.time;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
-}
