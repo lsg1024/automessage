930e612904a616af035017568a3f8d11874557bd
JS <zks145@naver.com>
2024-08-26 21:31:17 +0900

    fix: 에러 코드 수정

--- src/test/java/excel/automessage/BaseTest.java
+++ src/test/java/excel/automessage/BaseTest.java
@@ -1,5 +1,6 @@
 package excel.automessage;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -7,5 +8,6 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-api.properties")
 @ActiveProfiles("test")
+@Slf4j
 public abstract class BaseTest {
 }
