1d0bb65152879c88bb004e11f97df8a07f13d522
JS <zks145@naver.com>
2025-03-27 14:20:12 +0900

    fix: ip 주소 확인 로그 추가

--- src/main/java/excel/automessage/config/handler/InternalNetworkFilter.java
+++ src/main/java/excel/automessage/config/handler/InternalNetworkFilter.java
@@ -18,17 +18,17 @@ public class InternalNetworkFilter extends OncePerRequestFilter {
         String remoteAddr = request.getRemoteAddr();
         String requestURI = request.getRequestURI();
 
-        String remoteAdd2 = getClientIp(request);
-
-        log.info("접속 ip {} {}", remoteAddr, remoteAdd2);
+        log.info("접속 ip {}", remoteAddr);
 
         // 로그인 URL 제외
         if (requestURI.contains("/login")) {
+            log.info("로그인 URL 제외");
             filterChain.doFilter(request, response);
             return;
         }
 
         if (!isInternalNetwork(remoteAddr)) {
+            log.info("외부 ip 주소 입니다");
             response.sendRedirect("/login");
             return;
         }
@@ -38,17 +38,6 @@ public class InternalNetworkFilter extends OncePerRequestFilter {
 
     private boolean isInternalNetwork(String ip) {
         return ip.startsWith("192.168.") || ip.startsWith("10.") || ip.startsWith("172.") || ip.startsWith("127.") || ip.equals("0:0:0:0:0:0:0:1");
-    }
-
-    private String getClientIp(HttpServletRequest request) {
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("X-Real-IP");
-        }
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
     }
 
 }
